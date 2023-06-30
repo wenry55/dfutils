f5e97402938e14418aaba30e85fb9179474f5b23
SEO BONG-KYO <wenry55@gmail.com>
2023-06-30 11:01:00 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfutils',
-    version='1.0',
+    version='1.0.1',
     description='Dataframe utilities',
     author='B.K-SEO',
     packages=find_packages(),
